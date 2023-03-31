6ab2fb95b9071cf55aef46e1b9617947d6f56be3
LeonV1110 <59734771+LeonV1110@users.noreply.github.com>
2023-03-31 14:55:25 +0200

    comments

--- Prolog/test.pl
+++ Prolog/test.pl
@@ -1,12 +1,23 @@
 %Facts
-constelation(GEO)
-constelation(MEO)
-frequency_band(GEO, C)
-frequency_band(GEO, KA)
-frequency_band(GEO, KU)
-frequency_band(MEO, KA)
-
-
+constellation(geo)
+constellation(meo)
+frequency_band(geo, c)
+frequency_band(geo, ka)
+frequency_band(geo, ku)
+frequency_band(meo, ka) 
+service(mobile_backhaul)
 
 %Rules
+
+% 
+
+
+%inputs:
+% Bandwidth - Low, Medium, High
+% location - Urban, Rural, Remote, Ocean, Variable
+% Coordinates - 
+% latency - 
+% Edgecompute - Yes, No
+% Price - 
+
 
